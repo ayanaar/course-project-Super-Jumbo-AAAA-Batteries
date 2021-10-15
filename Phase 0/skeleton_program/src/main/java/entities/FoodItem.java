33615ff8fc0ccfb0f14b7a34fc6b1a03926888e8
jennicao <73912656+jennicao@users.noreply.github.com>
2021-10-15 19:31:04 -0400

    Update FoodItem.java
    Fixed indentation

--- Phase 0/skeleton_program/src/main/java/entities/FoodItem.java
+++ Phase 0/skeleton_program/src/main/java/entities/FoodItem.java
@@ -6,45 +6,45 @@ package entities;
  * and expiry date.
  */
 public class FoodItem extends Item {
-    private int expiryDate;
+	private int expiryDate;
 
-    /**
-     * Initialize a FoodItem with the given name, quantity,
-     * and expiry date.
-	 *
-     * @param name          name of food item.
-     * @param quantity      quantity of food item.
-     * @param expiryDate    expiry date of the food item.
-     */
-    public FoodItem(String name, int quantity, int expiryDate) {
-        super(name, quantity);
-        this.expiryDate = expiryDate;
-    }
+	/**
+	* Initialize a FoodItem with the given name, quantity,
+	* and expiry date.
+	*
+	* @param name          name of food item.
+	* @param quantity      quantity of food item.
+	* @param expiryDate    expiry date of the food item.
+	*/
+	public FoodItem(String name, int quantity, int expiryDate) {
+		super(name, quantity);
+		this.expiryDate = expiryDate;
+	}
 
-    /**
-     * Return the expiry date of the food item.
-	 *
-     * @return the expiry date of the food item.
-     */
+	/**
+	* Return the expiry date of the food item.
+	*
+	* @return the expiry date of the food item.
+	*/
 	public int getExpiryDate() {
 		return this.expiryDate;
 	}
 
-    /**
-     * Update the expiry date of the food item (decrease by one day).
-     */
-    public void updateExpiryDate() {
-        this.expiryDate -= 1;
-    }
+	/**
+	* Update the expiry date of the food item (decrease by one day).
+	*/
+	public void updateExpiryDate() {
+		this.expiryDate -= 1;
+	}
 
-    /**
-     * Return a string representation of the food item.
-	 *
-     * @return a string representation of the food item.
-     */
+	/**
+	* Return a string representation of the food item.
+	*
+	* @return a string representation of the food item.
+	*/
 	@Override
 	public String toString() {
 		return "Name: " + super.getName() + " Quantity: " + super.getQuantity() +
-                " Expiry Date: " + this.expiryDate;
+			" Expiry Date: " + this.expiryDate;
 	}
 }
