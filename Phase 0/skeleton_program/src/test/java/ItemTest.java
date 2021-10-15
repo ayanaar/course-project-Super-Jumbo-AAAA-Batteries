c9b498903e7225322bc08158e185b2a339963b3b
jennicao <73912656+jennicao@users.noreply.github.com>
2021-10-15 19:50:27 -0400

    Update ItemTest.java
    Fixed indentation

--- Phase 0/skeleton_program/src/test/java/ItemTest.java
+++ Phase 0/skeleton_program/src/test/java/ItemTest.java
@@ -1,7 +1,7 @@
 import entities.Item;
+
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 
@@ -11,28 +11,28 @@ import static org.junit.Assert.assertEquals;
 public class ItemTest {
   	Item item;
 
-    /**
+	/**
 	 * Set up for testing Item class.
 	 */
-    @Before
-    public void setUp() {
-        item = new Item("toilet paper", 99);
-    }
+	@Before
+	public void setUp() {
+        	item = new Item("toilet paper", 99);
+    	}
 
-    /**
+    	/**
 	 * Test that the toString method returns the expected string representation.
 	 */
-    @Test(timeout = 100)
-    public void testToString() {
-        assertEquals("Name: toilet paper Quantity: 99", item.toString());
-    }
+	@Test(timeout = 100)
+	public void testToString() {
+		assertEquals("Name: toilet paper Quantity: 99", item.toString());
+	}
 
-    /**
-     * Test that the updateQuantity method updates the item quantity to the expected amount.
-     */
-    @Test(timeout = 100)
-    public void testUpdateQuantity() {
-        item.updateQuantity(10);
-        assertEquals(109, item.getQuantity());
-    }
+	/**
+	 * Test that the updateQuantity method updates the item quantity to the expected amount.
+	 */
+	@Test(timeout = 100)
+	public void testUpdateQuantity() {
+		item.updateQuantity(10);
+		assertEquals(109, item.getQuantity());
+	}
 }
