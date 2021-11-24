1ff3a5574c33b6c28b195e3b5f932255795b45ff
alissalozhkin <alissa.lozhkin@gmail.com>
2021-11-24 17:06:20 -0500

    new folder

---
+++ Phase 2/src/main/java/use_cases/DataHandlingUseCase.java
@@ -0,0 +1,12 @@
+package use_cases;
+
+/**
+ * Interface for InventoryLists and ShoppingLists that can save data to the database.
+ */
+interface DataHandlingUseCase {
+
+    /**
+     * This method is for saving the current state of the list.
+     */
+    void saveList();
+}
---
+++ Phase 2/src/main/java/use_cases/ShoppingListUseCases.java
@@ -0,0 +1,56 @@
+package use_cases;
+
+import entities.ShoppingList;
+import entities.Item;
+import gateway.SavingManager;
+
+
+/**
+ * This class interacts with the shopping list entities.
+ */
+public class ShoppingListUseCases implements DataHandlingUseCase {
+	private final ShoppingList shoppingList;
+
+	/**
+	 * Initialize an empty shopping list.
+	 */
+	public ShoppingListUseCases() {
+		this.shoppingList = new ShoppingList();
+	}
+
+	/**
+	 * Return the user's shopping list that will be printed to the user.
+	 *
+	 * @return the user's shopping list.
+	 */
+	public String returnShoppingList() {
+		return this.shoppingList.toString();
+	}
+
+	/**
+	 * Add the given item (name and quantity) to the user's shopping list.
+	 *
+	 * @param name          the name of the item being added.
+	 * @param quantity      the quantity of the item being added.
+	 */
+	public void addShoppingListItem(String name, int quantity) {
+		Item item = new Item(name, quantity);
+		this.shoppingList.addItem(item);
+	}
+
+	/**
+	 * Remove the item at the given index from the user's shopping list.
+	 *
+	 * @param index the index of the item being removed.
+	 */
+	public void removeShoppingListItem(int index) {
+		this.shoppingList.removeItem(index);
+	}
+
+	/**
+	 * Save the shopping list to the csv file.
+	 */
+	public void saveList() {
+		SavingManager.ShoppingListSaver(this.shoppingList.toStringBuilder());
+	}
+}
